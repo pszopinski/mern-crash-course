0a61316db2f2262e9390b9cafd3f4b1a2d5e427c
Paweł Szopiński <contact@pawelszopinski.com>
2024-09-18 15:33:31 +0200

    Return 500 on exception while deleting a product
    Case for invalid ID is covered above.

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -68,6 +68,6 @@ export const deleteProduct = async (req, res) => {
     res.status(200).json({ success: true, message: "Product deleted" });
   } catch (error) {
     console.error(`Could not delete product: ${error.message}`);
-    res.status(404).json({ success: false, message: "Product not found" });
+    res.status(500).json({ success: false, message: "Server error" });
   }
 };
