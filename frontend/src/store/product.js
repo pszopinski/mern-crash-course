404e8a9cc76caac25f4b086683d01f43ad5cfe3d
Paweł Szopiński <contact@pawelszopinski.com>
2024-09-23 14:00:29 +0200

    Add Create Product form

---
+++ frontend/src/store/product.js
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+
+export const useProductStore = create((set) => ({
+  products: [],
+
+  setProducts(products) {
+    return set({ products });
+  },
+
+  async createProduct(newProduct) {
+    if (!newProduct.name || !newProduct.image || !newProduct.price) {
+      return { success: false, message: "Please fill in all the fields." };
+    }
+
+    const res = await fetch("/api/products", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newProduct),
+    });
+
+    const data = await res.json();
+    set((state) => ({ products: [...state.products, data.data] }));
+
+    return { success: true, message: "Product created successfully." };
+  },
+}));
