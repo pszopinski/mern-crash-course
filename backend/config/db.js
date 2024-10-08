660565842ead826f07e9e4fbec8ed808f303353c
Paweł Szopiński <contact@pawelszopinski.com>
2024-10-08 20:22:13 +0200

    Fix missing argument

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ export const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${connection.connection.host}`);
-  } catch {
+  } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
